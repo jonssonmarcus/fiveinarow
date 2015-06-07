4373fa304760b2dd2654b11dcebac1597f162587
MARCUS JÖNSSON <majd15@seekers.student.bth.se>
2015-06-07 15:46:25 +0200

    second commit

---
+++ fiveinarow/fiveinarow.php
@@ -0,0 +1,35 @@
+<?php $title='Template for testprogram'; include(__DIR__ . '/../../header.php'); ?>
+<div id="body">
+<div id="title">Five in a row</div>
+<p/>
+<div id="fiarimg"> <a href="http://www.student.bth.se/~majd15/kmom0710/fiveinarow/"><img src="fiveinarow.png" height="200" width="550" /></a></div>
+<p/>
+<div id="productinfo">Five in a row är ett väldigt underhållande spel som ibland
+benämns som luffarchack. Spelet är ett strategispel som går ut på att man ska försöka få 
+5 av sina egna markörer i en linje. Linjen får vara vågrät, lodrät eller diagonal. Den som 
+först får 5 markörer i rad vinner. Man kan välja att spela mot datorn eller så
+bjuder man in någon som är inloggad på spelservern. Den här versionen av 5-i-rad är helt
+reklamfri och då den är byggd med den senaste tekniken så behöver man aldrig uppdatera
+sin webbläsare för att se om det är någon som har loggat in eller ut från servern eller
+om någon har skickat en spelinbjudan.</div>
+</p>
+<div id="compare">
+Det finns flera versioner av five-in-a-row på nätet, men det som är bättre med denna
+versionen jämfört med många av de andra är att den är helt reklamfri. Eftersom 
+spel-instruktionerna finns på samma sida som spelet så är det också enklare att komma
+igång med min version jämfört med många andra versioner. Många av de versioner som finns
+erbjuder bara spel mot datorn, medans min version är mycket flexiblare och det är möjligt
+att antingen spela mot datorn eller mot en annan kompis.
+</div>
+<p/>
+<div id="github">Programkoden för spelet finns på Github
+<a href="https://github.com/jonssonmarcus/fiveinarow">Koden på github</a>
+</div>
+<p/>
+
+<div id="tothegame">
+<a href="http://www.student.bth.se/~majd15/kmom0710/fiveinarow/">Till spelet</a>
+</div>
+</body>
+
+<?php $path=__DIR__; ?>
